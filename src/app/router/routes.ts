c988bc4dd75b53c99787217a5f615fee2e8b7325
MiggRabbid <mig26@inbox.ru>
2025-12-26 10:21:10 +0300

    Fix: routing

--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -1,4 +1,4 @@
 export default {
-  MainPage: '/',
+  MainPage: '/test-task-TECCOD',
   NotFound: '/*',
 };
--- src/pages/ErrorBoundary/index.tsx
+++ src/pages/ErrorBoundary/index.tsx
@@ -1,10 +1,26 @@
+// Библиотеки
 import React from 'react';
 import { Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+// Логика
+import routes from '@/app/router/routes';
+// Компоненты
+import { Button, CustomIcon } from '@/shared/ui';
 
 const ErrorBoundary = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(routes.MainPage);
+  };
+
   return (
-    <Box className="min-h-full w-full p-10! text-center text-3xl font-bold">
+    <Box className="flex flex-col items-center justify-center gap-20 p-10! pt-40! text-center text-3xl font-bold">
       <p>Произошла ошибка</p>
+      <Button onClick={handleRedirect} size="lg">
+        На главную
+        <CustomIcon name="HomeRounded" />
+      </Button>
     </Box>
   );
 };
